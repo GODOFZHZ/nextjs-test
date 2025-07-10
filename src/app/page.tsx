f015a428d500321d4533911d437a51a51b1a93f0
283594 <2834594@qq.com>
2025-07-11 00:02:18 +0800

    feat:isEdgeRuntime

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 
-export default function Page() {
+export default function Page({ runtime, isEdgeRuntime }) {
   // Edge Runtime 会有 globalThis.EdgeRuntime
   console.log(process.env.NEXT_RUNTIME);
   console.log(globalThis?.EdgeRuntime); // Vercel Edge);
@@ -8,6 +8,8 @@ export default function Page() {
     <div>
       <h1>Hello, Next.js!{process.env.NEXT_RUNTIME}</h1>
       {globalThis?.EdgeRuntime}
+      <p>runtime:{runtime }</p>
+      <div>isEdgeRuntime:{isEdgeRuntime}</div>
     </div>
   );
 }
