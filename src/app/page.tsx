8e558eed5089866840c85c9a4e71b3c3a574d215
283594 <2834594@qq.com>
2025-07-11 00:04:33 +0800

    props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 
-export default function Page({ runtime }) {
+export default function Page(props) {
   // Edge Runtime 会有 globalThis.EdgeRuntime
   console.log(process.env.NEXT_RUNTIME);
   console.log(globalThis?.EdgeRuntime); // Vercel Edge);
@@ -8,7 +8,7 @@ export default function Page({ runtime }) {
     <div>
       <h1>Hello, Next.js!{process.env.NEXT_RUNTIME}</h1>
       {globalThis?.EdgeRuntime}
-      <p>runtime:{runtime }</p>
+      <p>runtime:{JSON.stringify(props) }</p>
     </div>
   );
 }
