eb37d215ed1d7071b0a03c355ac9e9965d0f4afd
283594 <2834594@qq.com>
2025-07-11 00:22:33 +0800

    feata:1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,29 @@
 
-export default function Page(props) {
+export default async function Page(props) {
   // Edge Runtime 会有 globalThis.EdgeRuntime
   console.log(process.env.NEXT_RUNTIME);
   console.log(globalThis?.EdgeRuntime); // Vercel Edge);
-  
+  const data = await fetchData();
   return (
     <div>
       <h1>Hello, Next.js!{process.env.NEXT_RUNTIME}</h1>
-      {globalThis?.EdgeRuntime}
       <p>runtime:{JSON.stringify(props) }</p>
+      {globalThis?.EdgeRuntime}123123
+      {data}
     </div>
   );
 }
+async function fetchData() {
+  // 模拟从数据库或 API 获取数据
+  console.log(process.env.NEXT_RUNTIME);
+  console.log(globalThis?.EdgeRuntime); // Vercel Edge);
+  return [
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+  ];
+}
+// const getData = async () => {
+//   fetch('/')
+// } 
 
-export const config = { runtime: 'edge' }
+export const runtime = 'edge';
