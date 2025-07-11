f3435cc8286924bf2e935a193335feff65c2e0c4
283594 <2834594@qq.com>
2025-07-11 10:49:44 +0800

    feat:1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,18 @@
-
+'use server'
 export default async function Page(props) {
-  // Edge Runtime 会有 globalThis.EdgeRuntime
-  console.log(process.env.NEXT_RUNTIME);
-  console.log(globalThis?.EdgeRuntime); // Vercel Edge);
   const data = await fetchData();
   return (
     <div>
       <h1>Hello, Next.js!{process.env.NEXT_RUNTIME}</h1>
-      <p>runtime:{JSON.stringify(props) }</p>
       {globalThis?.EdgeRuntime}123123
       {JSON.stringify(data)}
     </div>
   );
 }
 async function fetchData() {
-  // 模拟从数据库或 API 获取数据
   console.log(process.env.NEXT_RUNTIME);
   console.log(globalThis?.EdgeRuntime); // Vercel Edge);
-  return [
-    { id: 1, name: 'Item 1' },
-    { id: 2, name: 'Item 2' },
-  ];
+  const res = await fetch('https://dog.ceo/api/breeds/image/random')
+  return res.json()
 }
-// const getData = async () => {
-//   fetch('/')
-// } 
-
 // export const runtime = 'edge';
