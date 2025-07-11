b3d91c5dc244c524ccb47417f8f9e9253d56cd25
283594 <2834594@qq.com>
2025-07-11 16:47:48 +0800

    feat:!

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,17 +1,23 @@
-export const revalidate = 60
-export const dynamicParams = true // or false, to 404 on unknown paths
+// export const revalidate = 60
+// export const dynamicParams = true // or false, to 404 on unknown paths
 // export const dynamic = 'force-dynamic';
 export async function generateStaticParams() {
-  const posts = await Promise.resolve([{id:1},{id:2},{id:3},{id:4}])
+  const posts = await fetch('https://api.vercel.app/blog').then((res) =>
+    res.json()
+  )
   return posts.map((post) => ({
     id: String(post.id),
   }))
+  // const posts = await Promise.resolve([{id:1},{id:2},{id:3},{id:4}])
+  // return posts.map((post) => ({
+  //   id: String(post.id),
+  // }))
 }
 export default async function Page({
   params,
 }) {
   const { id } = await params
-  const post= await fetch(`https://api.vercel.app/blog/${id}`).then(
+  const post = await fetch(`https://api.vercel.app/blog/${id}`).then(
     (res) => res.json()
   )
   return (
