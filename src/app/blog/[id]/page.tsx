fad4fa1f445a76bc08f44c027ef54a1ed412e9cd
283594 <2834594@qq.com>
2025-07-11 14:15:40 +0800

    feat:1

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,25 +1,18 @@
 export const revalidate = 60
-// We'll prerender only the params from `generateStaticParams` at build time.
-// If a request comes in for a path that hasn't been generated,
-// Next.js will server-render the page on-demand.
 export const dynamicParams = true // or false, to 404 on unknown paths
- 
+export const dynamic = 'force-static'
 export async function generateStaticParams() {
-  console.log(' posts');
-  const posts = await fetch('https://api.vercel.app/blog').then((res) =>
-    res.json()
-  )
-  console.log(posts, ' posts');
-  
+  console.log('kkkkkk posts');
+  const posts = await Promise.resolve([{id:1},{id:2},{id:3},{id:4}])
+  console.log(posts, '-------------generateStaticParams');
   return posts.map((post) => ({
     id: String(post.id),
   }))
 }
-
 export default async function Page({
   params,
 }) {
-  console.log(' posts222');
+  console.log('posts222');
   const { id } = await params
   const post= await fetch(`https://api.vercel.app/blog/${id}`).then(
     (res) => res.json()
