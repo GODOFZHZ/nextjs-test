79d620b48034bf58b8e226d0d1f84765d9c2c091
283594 <2834594@qq.com>
2025-07-11 17:01:47 +0800

    feat:1

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -2,7 +2,7 @@
 // export const dynamicParams = true // or false, to 404 on unknown paths
 // export const dynamic = 'force-dynamic';
 export async function generateStaticParams() {
-  const posts = await fetch('https://api.vercel.app/blog').then((res) =>
+  const posts = await fetch('https://api.vercel.app/blog', {cache: 'no-store'}).then((res) =>
     res.json()
   )
   return posts.map((post) => ({
@@ -17,7 +17,7 @@ export default async function Page({
   params,
 }) {
   const { id } = await params
-  const post = await fetch(`https://api.vercel.app/blog/${id}`).then(
+  const post = await fetch(`https://api.vercel.app/blog/${id}`, {cache: 'no-store'}).then(
     (res) => res.json()
   )
   return (
