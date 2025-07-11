9ee8f56f0cbfa44e956528bee72e052839c6c7e1
283594 <2834594@qq.com>
2025-07-11 16:52:49 +0800

    feat:!

--- src/app/home/pages.tsx
+++ src/app/home/pages.tsx
@@ -1,4 +1,12 @@
-
-export default () => {
-    return <>123</>
+export const revalidate = 60
+export default async function Page() {
+  const post = await fetch('https://api.vercel.app/blog').then(
+    (res) => res.json()
+  )
+  return (
+    <main>
+    <h1>ISR</h1>
+     {JSON.stringify(post)}
+    </main>
+  )
 }
---
+++ src/app/list/pages.tsx
@@ -0,0 +1,6 @@
+'use client'
+import { useState } from 'react'
+export default function Page() {
+  const [count, setCount] = useState(0)
+  return <button onClick={() => setCount(count + 1)}>{count} CSR</button>
+}
