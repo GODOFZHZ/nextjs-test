5f6e8455d7de46a3b5cd84f6bf6675df827f83e5
283594 <2834594@qq.com>
2025-07-11 17:33:29 +0800

    export const revalidate = 60

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,9 +1,9 @@
 // export const revalidate = 60
 // export const dynamicParams = true // or false, to 404 on unknown paths
 // export const dynamic = 'force-dynamic';
-export const revalidate = 60
+export const revalidate = 60;
 export async function generateStaticParams() {
-  const posts = await fetch('https://api.vercel.app/blog', { cache: 'no-store' }).then((res) =>
+  const posts = await fetch('https://api.vercel.app/blog').then((res) =>
     res.json()
   )
   return posts.map((post) => ({
