08ab65d1f7a1affa9e451b7acaf7525b498239b9
283594 <2834594@qq.com>
2025-07-11 14:26:37 +0800

    feat:123

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -2,6 +2,8 @@ export const revalidate = 60
 export const dynamicParams = true // or false, to 404 on unknown paths
 export const dynamic = 'force-static'
 export async function generateStaticParams() {
+  console.log(123123);
+  
   const posts = await Promise.resolve([{id:1},{id:2},{id:3},{id:4}])
   return posts.map((post) => ({
     id: String(post.id),
